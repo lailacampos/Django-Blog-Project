7ce34d3eb5eec2196c65ee42ab236f29a460c4c0
Laila Ribeiro Hamilton de Campos <laila.estagio@anatel.gov.br>
2022-05-11 12:25:32 -0300

    Add check for whether current user is author of post that is being updated

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,7 +4,7 @@
 
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 # Create your views here.
@@ -40,13 +40,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+
+        # Check if current user is author of the post:
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
