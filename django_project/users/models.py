97e2a4e6af7c457f024ad23bbcecb124180abde1
Laila Ribeiro Hamilton de Campos <laila.estagio@anatel.gov.br>
2022-05-14 11:41:30 -0300

    Comment overriden save method of Profile model in users/models.py

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -14,13 +14,13 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        # Resizing the image if larger than wanted:
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #
+    #     # Resizing the image if larger than wanted:
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
