6ff97b2f3ac403947110abacb04fd69bc552c909
Laila Ribeiro Hamilton de Campos <laila.estagio@anatel.gov.br>
2022-05-15 11:51:02 -0300

    Change DEBUG to True only in development. Change SECRET_KEY

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-adh2*yqtns&mdst5wl0^kxg4xf9y8jxs5&(bj(c^-(za=w&518'
+# SECRET_KEY = 'django-insecure-adh2*yqtns&mdst5wl0^kxg4xf9y8jxs5&(bj(c^-(za=w&518'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
